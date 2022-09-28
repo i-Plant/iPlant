a5415bdcd47a3029d2d9ee0d3aa1f4867a6e3d19
katygil7 <gil.katherine22@gmail.com>
2022-09-28 12:14:00 -0500

    first commit

---
+++ src/main/java/iplant/IPlantApplication.java
@@ -0,0 +1,13 @@
+package iplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPlantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IPlantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iplant/IPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package iplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPlantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
