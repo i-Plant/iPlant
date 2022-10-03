98c2187df713ecac6cec33b14b125663e7a02cf5
Louie Espinosa <louie.espinosa@proton.me>
2022-10-03 11:04:02 -0500

    added bootstrap grid divs to login view

--- src/main/resources/static/js/views/Login.js
+++ src/main/resources/static/js/views/Login.js
@@ -6,16 +6,33 @@ export default function Login(props) {
     <title>Log In</title>
 </head>
 <body>
-<h1>Log In</h1>
+<h1>Log In/Register</h1>
 
-<form id="login-form">
-    <label for="username">Username</label>
-    <input id="username" name="username" type="text"/>
-    <label for="password">Password</label>
-    <input id="password" name="password" type="password"/>
-    <input id="login-btn" type="submit" value="Log In"/>
-</form>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12-sm">
+            <form id="login-form">
+            
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text"/>
+                
+                <label for="email">Email</label>
+                <input id="email" name="email" type="text"/>
+                
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password"/>
+                <input id="login-btn" type="submit" value="Log In"/>
+                
+            </form>
+            <a href="/" data-link>Forgot Password</a>
+        </div>
+    </div>
+</div>
 </body>
+<footer>
+<a href="/about" data-link>I.Plant About Us</a>
+</footer>
 </html>`;
 
 }
