e425bc1b4bb65aaccedfd2aa2a21878f089efa21
Justin Quinn <Justin.Quinn09@gmail.com>
2022-10-03 11:19:52 -0500

    added Profile.js to views folder

---
+++ src/main/resources/static/js/views/Profile.js
@@ -0,0 +1,17 @@
+import CreateView from "../createView.js"
+
+export default function Register(props) {
+    return `
+    <header>
+        <h1>Checkout</h1>
+    </header>
+    
+    
+    
+    
+    `
+}
+
+export function ProfileEvent(){
+
+}
