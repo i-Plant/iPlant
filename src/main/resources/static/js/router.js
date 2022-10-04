f1b50b38d497d9eaf217c1eb869c960461966edc
Louie Espinosa <louie.espinosa@proton.me>
2022-10-04 11:35:22 -0500

    Added Google API script to login and index.html, along with private_constants.js to allow user to login with Goggle account

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -3,8 +3,8 @@ import PostIndex from "./views/PostIndex.js";
 import About from "./views/About.js";
 import Error404 from "./views/Error404.js";
 import Loading from "./views/Loading.js";
-import Login from "./views/Login.js";
-import LoginEvent from "./auth.js";
+import Login, {LoginEvent} from "./views/Login.js";
+//import LoginEvent from "./auth.js";
 import Register from "./views/Register.js"
 import {RegisterEvent} from "./views/Register.js";
 
