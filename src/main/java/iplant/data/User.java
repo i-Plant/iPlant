c6d6673e6c3c468bf9d466e0dcbc9701b53ed3ad
Justin Quinn <Justin.Quinn09@gmail.com>
2022-10-04 16:01:28 -0500

    added the Comment class in data folder

--- src/main/java/iplant/data/User.java
+++ src/main/java/iplant/data/User.java
@@ -20,14 +20,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
+    @NotNull
     @Column(nullable = false, unique = true, length = 100)
     private String userName;
     @Column(nullable = false, length = 100)
     private String firstName;
     @Column(nullable = false, length = 100)
     private String lastName;
-
     @Email
     @NotEmpty
     @Column(nullable = false, length = 100)
@@ -38,21 +37,17 @@ public class User {
     private String city;
     @Column()
     private String state;
-    @Column()
+    @Column(length = 5)
     private Integer zipCode;
-
     @ToString.Exclude
     @Column(nullable = false, length = 100)
     private String password;
-
     @Column()
     private LocalDate createdAt;
-
     @NotNull
     @Enumerated(EnumType.STRING)
     @Column
     private UserRole role;
-
 //    @OneToMany(mappedBy = "author")
 //    @JsonIgnoreProperties("author")
 //    private Collection<Post> posts;
