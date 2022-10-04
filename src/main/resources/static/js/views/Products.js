b6e70eea53a3b573ce47f2e3ba7e7bce6cfd4df1
katygil7 <gil.katherine22@gmail.com>
2022-10-04 10:23:09 -0500

    products view

--- src/main/resources/static/js/views/Products.js
+++ src/main/resources/static/js/views/Products.js
@@ -9,16 +9,29 @@ export default function Products(props) {
     for (let i = 0; i <products.length; i++){
         html += `
            <div class = "flip-card">      
-           <div class="flip-card-inner">
-           <div class="flip-card-front">
-           <
-</div>
-           
-           </div>
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="../../assets/${products[i].id}.jpg" alt="avatar" class="images">
+                    </div>
+                        <div class="flip-card-back">
+                            <p class="productInfo">
+                                <h1>${products[i].name}</h1>
+                                <h3>${products[i].scientific_name}</h3>
+                                <h4>${products[i].description}</h4>
+                            </p>
+                        </div>
+                </div>
            </div> 
         `
     }
+    return html;
 }
 export function ProductsEvent(){
-    console.log("test");
+    // fetch("https://www.growstuff.org/api/v1/crops").then((re) => re.json()).then((data) => console.log(data))
+
+
 }
+
+
+
+
