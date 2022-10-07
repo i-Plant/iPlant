e805d5adb186a771c5ba595e9174de67029d0c0e
Louie Espinosa <louie.espinosa@proton.me>
2022-10-07 11:59:23 -0500

    Pulled latest from main, and added a pom dependency for plant net, some small changes in fetchData.js

--- src/main/resources/static/js/public_constants.js
+++ src/main/resources/static/js/public_constants.js
@@ -1,2 +1,2 @@
-const GOOGLE_CLIENT_ID = '909908356091-jqa0i9r2adu6am96k5d43ms5lktiiboi.apps.googleusercontent.com'
-const GOOGLE_CLIENT_SECRET = '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'
+// const GOOGLE_CLIENT_ID = '909908356091-jqa0i9r2adu6am96k5d43ms5lktiiboi.apps.googleusercontent.com'
+// const GOOGLE_CLIENT_SECRET = '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'
