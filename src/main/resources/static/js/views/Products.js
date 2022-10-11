4ec147aed7ec06bb40899b834fe3a183a8d65b08
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-10-11 12:29:11 -0500

    Added picture styles to fit on card

--- src/main/resources/static/js/views/Products.js
+++ src/main/resources/static/js/views/Products.js
@@ -5,12 +5,13 @@ export default function Products(props) {
     <div>
         <h1>Our Plants</h1>
     </div>
+    <main>
     
    `
     for (let i = 0; i <products.length; i++){
-        html += `<main>
+        html += `
                     <div class="card" class="d-flex flex-wrap align-content-center" style="width:18rem">
-                      <img class="card-img-top" src="${products[i].imageURL}" alt="plant-image" class="images">
+                      <img class="card-img-top" src="${products[i].imageURL}" alt="plant-image" style="object-fit: fill;width: 100%; height: 250px"class="images">
                       <div class="card-body">
                         <h2>${products[i].name}</h2>
                         <p class="card-text">${products[i].details}</p>
@@ -19,10 +20,11 @@ export default function Products(props) {
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                       </div>
                     </div>
-               </main>
+               
 `
     }
-    return html;
+
+    return html+= `</main>`;
 }
 //<main class="d-flex flex-wrap align-content-center" id="main">
 export function ProductsEvent(){
