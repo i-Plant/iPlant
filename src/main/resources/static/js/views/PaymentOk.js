f5046c65feb23a3616e504b95a00a96214d7de90
katygil7 <gil.katherine22@gmail.com>
2022-10-17 12:27:42 -0500

    payment mesagge updated to display users name after check out

--- src/main/resources/static/js/views/PaymentOk.js
+++ src/main/resources/static/js/views/PaymentOk.js
@@ -1,13 +1,17 @@
+import {getUser} from "../auth.js";
+
 let users = [];
 export default function PaymentOk(props) {
-    users = props.user
+    users = props.users
+    const user = getUser();
+
     return `
         <header>
-            <h1>Payment Success</h1>
+            <h1>Payment Successful</h1>
         </header>
         <main> 
             <div>
-                <p> Thanks!!! Come Back ${users.id}</p>            
+                <p> Thanks!!! Come Back ${user.userName}</p>            
             </div>
         </main>
     `;
