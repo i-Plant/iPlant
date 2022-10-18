4073522d04b645caf772616e9b79b49e73c2f67c
NatalieSneed <106278703+NatalieSneed@users.noreply.github.com>
2022-10-18 15:05:29 -0500

    about us page

--- src/main/resources/static/js/views/About.js
+++ src/main/resources/static/js/views/About.js
@@ -1,49 +1,126 @@
 export default function About(props) {
     return `
-    <div class="container">
-<!--    <div class="wrapper"-->
-    
-    <div class="wrapper1">
-        <h1>About Us</h1>
-        <div>
-        <img src="https://i.etsystatic.com/21335212/c/1133/900/392/87/il/c02b8d/3058289202/il_340x270.3058289202_m2jb.jpg" alt="img">
-        <p>The Fab 5 life goes here ... </p>
+<div>  
+    <div class="container_about">
+  <div class="profile">
+    <div id="overlay">
+      <ul class="nav">
+        <li><a href="https://twitter.com/juliepark"  target="_blank"><i class="fab fa-twitter"></i> Twitter</a></li>
+        <li><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i> Facebook</a></li>
+        <li><a href="https://instagram.com/julieparkim"  target="_blank"><i class="fab fa-instagram"></i> Instagram</a></li>
+        <li><a href="https://github.com/jspark721"  target="_blank"><i class="fab fa-github"></i> Github</a></li>
+        <li><a href="https://codepen.io/juliepark"  target="_blank"><i class="fab fa-codepen"></i> Codepen</a></li>
+        <li><a href="https://www.linkedin.com/in/juliepark721/"  target="_blank"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
+      </ul>
+    </div>
+    <div class="top">
+      <div class="menu">
+        <div class="bar click-top"></div>
+        <div class="bar click-middle"></div>
+        <div class="bar click-bottom"></div>
+      </div>
+    </div>
+    <div class="bottom">
+      <h1>Julie Park</h1>
+      <h2>front-end developer</h2>
+      <p><i class="fas fa-location-arrow"></i> CodeUp</p>
+      <div class="line"></div>
+      <button class="margin-left">buy me coffee</button>
+      <button>follow</button>
         </div>
     </div>
-     
-     <div class="wrapper">
-        <h2>Faculties</h2>
-        <div class="faculties">
-            <div class="unit">
-            <img class="circular--square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFNd6M1RjPX8kLGTDVuKu8g9qrRuudDnbXOg&usqp=CAU" alt="">
-            <p>This Just In</p>
-            <p>Get's the job done</p>
-            
-        </div>
-        <div class="unit">
-        <img class="circular--square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQTeNQ1sDAEl3FxwrMPvNjgz9iiYAfjdXp2xCAk-ZugUoeWdl60XIa_d-Qacb2utS4r8A&usqp=CAU" alt="">
-            <p>Roger Dat</p>
-            <p>Yep Yep</p>
-            
-        </div>
-        <div class="unit">
-        <img class="circular--square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCG7L8ktlYJFpuReBX8k8ztMztDYyURoZM8D-57qTMmd6db-GZdnNrDLYz1P-vnesDl3U&usqp=CAU" alt="">
-            <p>Kat's out the bag</p>
-            <p>Da Boss</p>
-            
-        </div>
-        <div class="unit">
-        <img class="circular--square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWjicc6asMiPRwDmiJPXIHIBzfJIH7C4Mld3WwxHUfdmouMWCyE8v2nOelIN4xdHt7VZM&usqp=CAU" alt="">
-            <p>Louie Louie</p>
-            <p>Scoot on</p>
-            
-        </div>
-        <div class="unit">
-        <img class="circular--square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSttD1B8D3WeErWWhJFipQPr_puEO6Qb0W3FA&usqp=CAU" alt="">
-            <p>Nat Nat</p>
-            <p>Just chillin'</p>
-            
+  <div class="text">
+    <h2>click the menu to see animation</h2>
         </div>
     </div>
+    <div class="background"></div>
+
+
+<!--second card-->
+
+    <div class="container_about">
+  <div class="profile">
+    <div id="overlay">
+      <ul class="nav">
+        <li><a href="https://twitter.com/juliepark"  target="_blank"><i class="fab fa-twitter"></i> Twitter</a></li>
+        <li><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i> Facebook</a></li>
+        <li><a href="https://instagram.com/julieparkim"  target="_blank"><i class="fab fa-instagram"></i> Instagram</a></li>
+        <li><a href="https://github.com/jspark721"  target="_blank"><i class="fab fa-github"></i> Github</a></li>
+        <li><a href="https://codepen.io/juliepark"  target="_blank"><i class="fab fa-codepen"></i> Codepen</a></li>
+        <li><a href="https://www.linkedin.com/in/juliepark721/"  target="_blank"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
+      </ul>
+    </div>
+    <div class="top">
+      <div class="menu">
+        <div class="bar click-top"></div>
+        <div class="bar click-middle"></div>
+        <div class="bar click-bottom"></div>
+      </div>
+    </div>
+    <div class="bottom">
+      <h1>Julie Park</h1>
+      <h2>front-end developer</h2>
+      <p><i class="fas fa-location-arrow"></i> seattle, washington</p>
+      <div class="line"></div>
+      <button class="margin-left">buy me coffee</button>
+      <button>follow</button>
+    </div>
+  </div>
+  <div class="text">
+    <h2>click the menu to see animation</h2>
+  </div>
+</div>
+<div class="background"></div>
+
+</div>
+
+<!--<footer>-->
+<!--  <p>made by <a href="https://codepen.io/juliepark"> julie</a> ♡-->
+<!--</footer>-->
+<!--    <div class="container">-->
+<!--&lt;!&ndash;    <div class="wrapper"&ndash;&gt;-->
+<!--    -->
+<!--    <div class="wrapper1">-->
+<!--        <h1>About Us</h1>-->
+<!--        <div>-->
+<!--        <img src="https://i.etsystatic.com/21335212/c/1133/900/392/87/il/c02b8d/3058289202/il_340x270.3058289202_m2jb.jpg" alt="img">-->
+<!--        <p>The Fab 5 life goes here ... </p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--     -->
+<!--     <div class="wrapper">-->
+<!--        <h2>Faculties</h2>-->
+<!--        <div class="faculties">-->
+<!--            <div class="unit">-->
+<!--            <img class="circular&#45;&#45;square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFNd6M1RjPX8kLGTDVuKu8g9qrRuudDnbXOg&usqp=CAU" alt="">-->
+<!--            <p>This Just In</p>-->
+<!--            <p>Get's the job done</p>-->
+<!--            -->
+<!--        </div>-->
+<!--        <div class="unit">-->
+<!--        <img class="circular&#45;&#45;square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQTeNQ1sDAEl3FxwrMPvNjgz9iiYAfjdXp2xCAk-ZugUoeWdl60XIa_d-Qacb2utS4r8A&usqp=CAU" alt="">-->
+<!--            <p>Roger Dat</p>-->
+<!--            <p>Yep Yep</p>-->
+<!--            -->
+<!--        </div>-->
+<!--        <div class="unit">-->
+<!--        <img class="circular&#45;&#45;square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCG7L8ktlYJFpuReBX8k8ztMztDYyURoZM8D-57qTMmd6db-GZdnNrDLYz1P-vnesDl3U&usqp=CAU" alt="">-->
+<!--            <p>Kat's out the bag</p>-->
+<!--            <p>Da Boss</p>-->
+<!--            -->
+<!--        </div>-->
+<!--        <div class="unit">-->
+<!--        <img class="circular&#45;&#45;square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWjicc6asMiPRwDmiJPXIHIBzfJIH7C4Mld3WwxHUfdmouMWCyE8v2nOelIN4xdHt7VZM&usqp=CAU" alt="">-->
+<!--            <p>Louie Louie</p>-->
+<!--            <p>Scoot on</p>-->
+<!--            -->
+<!--        </div>-->
+<!--        <div class="unit">-->
+<!--        <img class="circular&#45;&#45;square" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSttD1B8D3WeErWWhJFipQPr_puEO6Qb0W3FA&usqp=CAU" alt="">-->
+<!--            <p>Nat Nat</p>-->
+<!--            <p>Just chillin'</p>-->
+<!--            -->
+<!--        </div>-->
+<!--    </div>-->
     `;
 }
