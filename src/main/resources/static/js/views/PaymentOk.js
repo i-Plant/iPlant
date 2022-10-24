256a71a718c94de29285bc8ed46a067b967770bc
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-10-23 19:26:17 -0500

    Payment Ok view and styling

--- src/main/resources/static/js/views/PaymentOk.js
+++ src/main/resources/static/js/views/PaymentOk.js
@@ -10,9 +10,31 @@ export default function PaymentOk(props) {
             <h1>Payment Successful</h1>
         </header>
         <main> 
-            <div>
-                <p> Thanks!!! Come Back ${user.userName}</p>            
-            </div>
+         <div class="popup">
+<!--            <button id="close">&times;</button>-->
+            <h2>Payment Successful</h2>
+            <img src="../../assets/paymentOk.png" alt="payment-ok">
+             <p> Thanks You For Shopping With Us ${user.userName}, Come Back Soon!</p>            
+            <a data-link href="/products" class="payment-ok">Shop More</a>
+         </div>
+            
         </main>
     `;
+
+}
+
+export function PaymentOkEvent() {
+    window.addEventListener("load", function(){
+        setTimeout(
+            function open(event){
+                document.querySelector(".popup").style.display = "block";
+            },
+            1000
+        )
+    });
+
+
+    document.querySelector("#close").addEventListener("click", function(){
+        document.querySelector(".popup").style.display = "none";
+    });
 }
