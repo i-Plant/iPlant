82baea87815b14d9e02d72f8f455a82d08a5592c
Louie Espinosa <louie.espinosa@proton.me>
2022-10-25 09:57:05 -0500

    minor testing of cart and home css

--- src/main/resources/static/js/views/Cart.js
+++ src/main/resources/static/js/views/Cart.js
@@ -21,19 +21,18 @@ export default function addToCart(props) {
 
             cardsHTML += `
             <!--For the checkout button-->
-            <a data-link href="/checkout" id="checkout-btn"><i data-passthru class="fa-solid fa-dollar-sign">Checkout</i></a>
-             
             <div class="container cart-container">
+                <div class="delete-cart">
+                    <h2>Total Bill: $</h2>
+                    <button data-passthru id="checkoutBtn" class="checkout">Checkout</button>
+                    <button class="removeAll">Clear Cart</button>
+                </div>
             `;
             products = myOrder.products;
             for (let j = 0; j < products.length; j++) {
 
                 cardsHTML += `       
-       
-                 <h2>Total Bill: $ ${products[j].item.price}</h2>
-                 <button class="checkout">Checkout</button>
-                 <button onclick="clearCart()" class="removeAll">Clear Cart</button>
-                
+                           
                  <div class="cart-item">                
                      <img src="${products[j].item.imageURL}" alt="A plant" style="width:35%" >
                      <div class="details">
@@ -60,16 +59,16 @@ export default function addToCart(props) {
                 
                 `;
                 let total = 0;
-                for (let k = 0; k < products.length; k++) {
-                    total += products[k].quantity;
+                for (let k = 0; k < products[j].length; k++) {
+                    total += products[k].quantity.reduce(total);
                 }
                 cardsHTML += `
- <!--For the shopping cart icon-->
- <div class="cart">
-    <a data-link href="/cart"><i data-passthru id="cart" class="fa-solid fa-cart-shopping"></i></a>
-    <!--This is the cart total item counter-->
-    <div class="cart-amount">${total}</div>
- </div>
+                 <!--For the shopping cart icon-->
+                 <div class="cart">
+                    <a data-link href="/cart"><i data-passthru id="cart" class="fa-solid fa-cart-shopping"></i></a>
+                    <!--This is the cart total item counter-->
+                    <div class="cart-amount">${total}</div>
+                 </div>
 
                  
                  <div id='label' class='text-center'></div>
@@ -96,9 +95,10 @@ export default function addToCart(props) {
 // console.log(basket);
 
 export function addToCartEvent() {
-    //  calculation();
+
     //  totalAmount();
-    //  clearCart();
+    checkout();
+    clearCart();
     addIncrementDecrementHandlers();
     setupDeleteHandlers();
 
@@ -223,84 +223,18 @@ function setupDeleteHandlers() {
         });
     }
 }
-//for the badge counter in the cart
-function calculation() {
-    let cartCounter = document.querySelector(".cart-amount");
-    cartCounter.innerHTML = products.map((x) => x.item).reduce((x, y) => x + y, 0);
+function checkout() {
+   const checkoutBtn = document.querySelector("#checkoutBtn");
+   checkoutBtn.addEventListener("click", function(){
+       createView("/checkout")
+   })
 }
-// function pseudoDelete () {
-//     const deleteX = document.querySelectorAll(".cart-item")
-//     for(let i = 0; i < deleteX.length; i++) {
-//         deleteX[i].addEventListener("click", function () {
-//             deleteX[i].classList.toggle("remove-item")
-//         })
-//     }
-// }
-
-
-
-
-
-//Add all the product items as a total sum of items to be displayed where needed, i.e., the cart badge, the total sum,
-
-
-// //If cart is empty do NOT allow checkout button --> checkout page and inform the user
-// function validateCheckout() {
-//     let isValid = true;
-//     let completeCheckout = document.querySelector("#checkout-btn");
-//     if(completeCheckout.length < 1) {
-//         completeCheckout.classList.add("order is-invalid");
-//         completeCheckout.classList.remove("is-valid");
-//         isValid = false;
-//     } else {
-//        completeCheckout.classList.add("is-valid");
-//         completeCheckout.classList.remove("order is-invalid");
-//     }
-//
-//     return isValid;
-// }
-// if (!validateCheckout()) {
-//     return;
-// }
-
-
-// shoppingCart.innerHTML = ``;
-// label.innerHTML = `
-//          <h2>Cart is Empty</h2>
-//          <a style="margin-top: 50px" data-link href="/products">
-//              <button data-link class="products">Back to shopping</button>
-//          </a>
-//          `;
-
-
-//Id like to tell the user they can't checkout when cart is empty
-
-// function setupValidationHandlers() {
-//     let checkout = document.querySelector("")
-//     checkout.addEventListener("click", validateOrder);
-//
-// }
-// //If cart is empty do NOT allow checkout button --> checkout page and inform the user
-// function validateCheckout() {
-//     let isValid = true;
-//     let completeCheckout = document.querySelector("#checkout-btn");
-//     if(completeCheckout.length < 1) {
-//         completeCheckout.classList.add("order is-invalid");
-//         completeCheckout.classList.remove("is-valid");
-//         isValid = false;
-//     } else {
-//        completeCheckout.classList.add("is-valid");
-//         completeCheckout.classList.remove("order is-invalid");
-//     }
-//
-//     return isValid;
-// }
-// if (!validateCheckout()) {
-//     return;
-// }
-
-
-
+let clearCart = () => {
+    let banish =  document.querySelector(".removeAll")
+    banish.addEventListener("dblClick", function(){
+        this.outerHTML.childElementCount = [];
+    })
+}
 
 //increment and decrement functions
 // let bucket;
@@ -328,46 +262,7 @@ function calculation() {
 //     bucket.quantity--;
 // }
 
-//TODO: this function works with front end storage.
-// let increment = (id) => {
-//     let search = basket.find((x) => x.props.orders.id === props.orders.id);
-//     //if the product is already in the basket, dont push the entire object, rather increment only the item#
-//     if(search === undefined){basket.push({
-//         id: props.orders.id,
-//         item: 1,
-//     })
-//     } else {
-//         search.item += 1;
-//     }
-//     //saving the increment to local storage immediately; same is done to decrement.
-//     update(props.orders.id);
-//     //rerender the cart after the updates
-//     generateCartItems();
-//     localStorage.setItem("data", JSON.stringify(basket));
-// }
-//I want decrement to remove quantity of items and then complete;ly remove the card when it reaches 0
-// let decrement = (id) => {
-//     let search = basket.find((x) => x.id === selectedItem.id);
-//     //if the product is not in the basket, don't decrement anymore.
-//     if (search === undefined) return
-//     else if (search.item === 0) return;
-//     else {
-//         search.item -= 1;
-//     }
-//     update(selectedItem.id);
-//     //while there are no items do not display
-//     basket = basket.filter((x) => x.item !== 0);
-//
-//     generateCartItems();
-//     localStorage.setItem("data", JSON.stringify(basket));
-// }
 
-let clearCart = () => {
-   let banish =  document.querySelector(".cart-container")
-    banish.addEventListener("dblClick", function(){
-        this.outerHTML
-    })
-}
 
 let totalAmount = () => {
     if (basket.products.length !== 0) {
